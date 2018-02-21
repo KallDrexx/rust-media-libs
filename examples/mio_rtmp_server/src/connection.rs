c9663a05744566e3db09d727487954bce6d74d86
KallDrexx <me@mshapiro.net>
2018-02-20 20:29:07 -0500

    Created demo tcp echo server

---
+++ examples/mio_rtmp_server/src/connection.rs
@@ -0,0 +1,125 @@
+use std::io;
+use std::io::{Read, Write};
+use std::collections::VecDeque;
+use mio::{Token, Ready, Poll, PollOpt};
+use mio::net::TcpStream;
+
+pub struct Connection {
+    socket: TcpStream,
+    pub token: Option<Token>,
+    interest: Ready,
+    send_queue: VecDeque<Vec<u8>>,
+    read_buffer: Vec<u8>,
+    has_been_registered: bool,
+}
+
+impl Connection {
+    pub fn new(socket: TcpStream) -> Connection {
+        Connection {
+            socket,
+            token: None,
+            interest: Ready::readable() | Ready::writable(),
+            send_queue: VecDeque::new(),
+            read_buffer: Vec::new(),
+            has_been_registered: false,
+        }
+    }
+
+    pub fn write_line(&mut self, poll: &mut Poll, line: Vec<u8>) -> io::Result<()> {
+        self.send_queue.push_back(line);
+        self.interest.insert(Ready::writable());
+        self.register(poll)?;
+        Ok(())
+    }
+
+    pub fn readable(&mut self, poll: &mut Poll) -> io::Result<()> {
+        println!("Socket readable");
+
+        let mut buffer = [0_u8; 1024];
+        loop {
+            match self.socket.read(&mut buffer) {
+                Ok(bytes_read) => {
+                    println!("{} bytes read", bytes_read);
+
+                    if bytes_read == 0 {
+                        break;
+                    }
+
+                    for x in 0..bytes_read {
+                        self.read_buffer.push(buffer[x]);
+
+                        if buffer[x] == '\n' as u8 {
+                            // Send the line back to the client
+                            let line = self.read_buffer.drain(..).collect();
+                            self.write_line(poll, line)?;
+                        }
+                    }
+                },
+
+                Err(error) => {
+                    if error.kind() == io::ErrorKind::WouldBlock {
+                        // Can't read right now, so wait till next readable event
+                        break;
+                    } else {
+                        println!("Failed to send buffer for {:?} with error {}", self.token, error);
+                        return Err(error);
+                    }
+                }
+            }
+        }
+
+        self.register(poll)?;
+        Ok(())
+    }
+
+    pub fn writable(&mut self, poll: &mut Poll) -> io::Result<()> {
+        println!("Socket writable");
+
+        let message = match self.send_queue.pop_front() {
+            Some(x) => x,
+            None => {
+                // Queue was empty, so we are no longer interested in writable events
+                self.interest.remove(Ready::writable());
+                self.register(poll)?;
+                return Ok(());
+            }
+        };
+
+        match self.socket.write(&message) {
+            Ok(bytes_sent) => {
+                println!("Sent message length of {} bytes", bytes_sent);
+            },
+
+            Err(error) => {
+                if error.kind() == io::ErrorKind::WouldBlock {
+                    // Client buffer is full, push it back to the queue
+                    println!("test");
+                    self.send_queue.push_front(message);
+                } else {
+                    println!("Failed to send buffer for {:?} with error {}", self.token, error);
+                    return Err(error);
+                }
+            }
+        };
+
+        if self.send_queue.is_empty() {
+            println!("Outbound queue is empty");
+            self.interest.remove(Ready::writable());
+        }
+
+        self.register(poll)?;
+        Ok(())
+    }
+
+    pub fn register(&mut self, poll: &mut Poll) -> io::Result<()> {
+        println!("Registering with interest: {:?}", self.interest);
+
+        match self.has_been_registered {
+            true => poll.reregister(&self.socket, self.token.unwrap(), self.interest, PollOpt::edge() | PollOpt::oneshot())?,
+            false => poll.register(&self.socket, self.token.unwrap(), self.interest, PollOpt::edge() | PollOpt::oneshot())?
+        }
+
+        self.has_been_registered = true;
+        Ok(())
+    }
+}
--- examples/mio_rtmp_server/src/main.rs
+++ examples/mio_rtmp_server/src/main.rs
@@ -1,30 +1,55 @@
 extern crate mio;
+extern crate slab;
+
+mod connection;
 
 use mio::*;
 use mio::net::{TcpListener};
+use slab::Slab;
+use ::connection::Connection;
 
-const SERVER: Token = Token(0);
+const SERVER: Token = Token(std::usize::MAX - 1);
 
 fn main() {
     let addr = "127.0.0.1:1935".parse().unwrap();
     let server = TcpListener::bind(&addr).unwrap();
-    let poll = Poll::new().unwrap();
+    let mut poll = Poll::new().unwrap();
+
+    println!("Listening for connections");
     poll.register(&server, SERVER, Ready::readable(), PollOpt::edge()).unwrap();
 
     let mut events = Events::with_capacity(1024);
+    let mut connections = Slab::new();
 
     loop {
         poll.poll(&mut events, None).unwrap();
 
         for event in events.iter() {
+            println!("ready {:?} {:?}", event.token(), event.readiness());
+            println!("token: {:?}", event.token());
+
             match event.token() {
                 SERVER => {
-                    let _ = server.accept();
-                    println!("test");
+                    let (socket, _) = server.accept().unwrap();
+                    let mut connection = Connection::new(socket);
+                    let token = connections.insert(connection);
+                    connections[token].token = Some(Token(token));
+                    connections[token].register(&mut poll).unwrap();
                 },
 
-                _ => {
-                    return;
+                Token(value) => {
+                    let mut connection = match connections.get_mut(value) {
+                        Some(connection) => connection,
+                        None => continue,
+                    };
+
+                    if event.readiness().is_readable() {
+                        connection.readable(&mut poll).unwrap();
+                    }
+
+                    if event.readiness().is_writable() {
+                        connection.writable(&mut poll).unwrap();
+                    }
                 }
             }
         }
