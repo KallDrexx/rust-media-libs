a415e7fff42bdfab04f9bd051184692f33cbc7da
Daniel Sedlak <daniel@sedlak.dev>
2023-03-28 14:49:01 +0000

    Fix test for checking chunk size after session creation
    In previous patches, we introduced a breaking change due to moving
    the message for setting the chunk size. Now the chunk size is set
    after a successful connection.

--- rtmp/src/sessions/client/tests.rs
+++ rtmp/src/sessions/client/tests.rs
@@ -8,13 +8,22 @@ use rml_amf0::Amf0Value;
 use std::collections::HashMap;
 
 #[test]
-fn new_session_creates_set_chunk_size_message() {
+fn new_session_and_successful_connect_creates_set_chunk_size_message() {
+    let app_name = "test".to_string();
     let mut config = ClientSessionConfig::new();
     config.chunk_size = 1111;
 
     let mut deserializer = ChunkDeserializer::new();
-    let (_, initial_results) = ClientSession::new(config.clone()).unwrap();
+    let mut serializer = ChunkSerializer::new();
+    let (mut session, initial_results) = ClientSession::new(config.clone()).unwrap();
     consume_results(&mut deserializer, initial_results);
+
+    perform_successful_connect(
+        app_name.clone(),
+        &mut session,
+        &mut serializer,
+        &mut deserializer,
+    );
 
     assert_eq!(
         deserializer.get_max_chunk_size(),
